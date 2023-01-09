dc9dfd1c1abfcc12b143f75d7a01839c2906670e
Renan Alves <109705192+RenanAlvesSilva@users.noreply.github.com>
2023-01-09 09:27:11 -0300

    Entrando no youtube e escolhendo um vídeo.
    Automação com python, entrando no youtube e escolhendo um vídeo.

---
+++ youtube.py
@@ -0,0 +1,43 @@
+import pyautogui as mouse
+import pyautogui as espera
+
+espera.sleep(2)
+mouse.moveTo(211, 747)
+
+espera.sleep(2)
+mouse.click(211, 747)
+
+espera.sleep(2)
+mouse.moveTo(410, 357)
+
+espera.sleep(2)
+mouse.click(418, 357)
+
+espera.sleep(2)
+mouse.moveTo(622, 518)
+
+espera.sleep(2)
+mouse.click(622, 518)
+
+espera.sleep(2)
+mouse.moveTo(501, 140)
+
+espera.sleep(2)
+mouse.click(501, 140)
+
+espera.sleep(4)
+mouse.typewrite('Funk do homem macaco')
+
+espera.sleep(2)
+mouse.press('enter')
+
+espera.sleep(2)
+mouse.moveTo(633, 656)
+
+espera.sleep(2)
+mouse.click(355, 537)
+
+
+# espera.sleep(5)
+
+# print(mouse.position())
